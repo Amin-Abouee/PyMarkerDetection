09d4abe5344e0db24b17368f5ea51910ac5b9875
Amin-Abouee <amin.abouee.mehrizi@in.tum.de>
2015-09-17 15:41:57 +0200

    fix the number of scanlines, restricted to cols and rows scan lines

--- marker_detection.py
+++ marker_detection.py
@@ -11,16 +11,22 @@ class MarkerDetection:
 	def image_division(self):
 		image_rows, image_cols = self.__image.shape[:2]
 		print self.__image.shape[:2]
-		self.__grid_indices = [np.array([y, x]) for x in xrange (0, 
-			image_cols, self.__GRID_SIZE) for y in xrange(0, image_rows, self.__GRID_SIZE)]
-
-		# print len(self.__grid_indices)
+		cols_scanline_positions = [np.array ([0, x], dtype = np.float32) for x in xrange (0, image_cols, self.__GRID_SIZE)]
+		rows_scanline_positions = [np.array ([y, 0], dtype = np.float32) for y in xrange (self.__GRID_SIZE, image_rows, self.__GRID_SIZE)]
+		self.__scanline_positions = cols_scanline_positions + rows_scanline_positions
+		# print len(self.__scanline_positions)
+		# print self.__scanline_positions
 		self.__pool = multiprocessing.Pool()
+
 
 	@property
 	def image(self):
 	    return self.__image
 
+	@property
+	def scanline_positions(self):
+	    return self.__scanline_positions
+	
 	@image.setter
 	def image(self, image):
 		self.__image = image
