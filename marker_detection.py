d543666644ec62e0d0337f16146941dd177212a2
Amin-Abouee <amin.abouee.mehrizi@in.tum.de>
2015-09-17 18:43:47 +0200

    add multiprocessing pool for each grid, add grid division

--- marker_detection.py
+++ marker_detection.py
@@ -1,32 +1,46 @@
 import numpy as np
 import cv2
-import multiprocessing
+from pathos.multiprocessing import ProcessingPool as Pool
+
 
 class MarkerDetection:
 
-    def __init__(self, image, grid_size, baselines_size):
+    def __init__(self, image, grid_size, baselines_size, threshold):
         self.__image = image
         self.__sobel_x = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5, scale=0.0625)
         self.__sobei_y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5, scale=0.0625)
         self.__GRID_SIZE = grid_size
         self.__BASELINES_SIZE = baselines_size
+        self.__THRESHOLD = threshold
+
+    def detect_edgle(self, pos):
+        pass
+
+    def grid_division(self, pos):
+        # include the border
+        bound_row = pos[0] + self.__GRID_SIZE + 1
+        bound_col = pos[1] + self.__GRID_SIZE + 1
+        baseline_positions = [np.array([x, y]) for x in xrange (pos[1], 
+            bound_col, self.__BASELINES_SIZE) for y in xrange(pos[0], 
+            bound_row, self.__BASELINES_SIZE)]
+
+        return len(baseline_positions),
 
     def image_division(self):
         image_rows, image_cols = self.__image.shape[:2]
         print self.__image.shape[:2]
-        self.__grid_indices = [np.array([x, y]) for x in xrange (0, 
-            image_cols - self.__GRID_SIZE, self.__GRID_SIZE) for y in xrange(0, image_rows - self.__GRID_SIZE, self.__GRID_SIZE)]
-        print self.__grid_indices
-        self.__pool = multiprocessing.Pool()
+        grid_indices = [np.array([x, y]) for x in xrange (0, 
+            image_cols - self.__GRID_SIZE, self.__GRID_SIZE) for y in xrange(0, 
+            image_rows - self.__GRID_SIZE, self.__GRID_SIZE)]
 
+        self.__pool = Pool()
+        output = self.__pool.map(self.grid_division, grid_indices)
+        print output
 
     @property
     def image(self):
         return self.__image
 
-    @property
-    def grid_indices(self):
-        return self.__grid_indices
     
     @image.setter
     def image(self, image):
